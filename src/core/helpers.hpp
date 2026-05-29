05609de0bfbfca8733bc614f83d68dedb63dbf76
Fury <fu.yu.447711@gmail.com>
2026-05-29 18:06:49 +0200

    Use glad 2 header in core helpers

--- src/core/helpers.hpp
+++ src/core/helpers.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <glad/glad.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
@@ -97,7 +97,7 @@ namespace bonobo
 
 	//! \brief Load an image into an OpenGL 2D-texture.
 	//!
-	//! @param [in] filename of the image.
+	//! @param [in] filename path to the texture to load
 	//! @param [in] generate_mipmap whether or not to generate a mipmap hierarchy
 	//! @return the name of the OpenGL 2D-texture
 	GLuint loadTexture2D(std::string const& filename,
@@ -118,101 +118,19 @@ namespace bonobo
                                   std::string const& posz, std::string const& negz,
                                   bool generate_mipmap = true);
 
-	//! \brief Create an OpenGL program consisting of a vertex and a
-	//!        fragment shader.
-	//!
-	//! @param [in] vert_shader_source_path of the vertex shader source
-	//!             code, relative to the `shaders/` folder
-	//! @param [in] frag_shader_source_path of the fragment shader source
-	//!             code, relative to the `shaders/` folder
-	//! @return the name of the OpenGL shader program
-	GLuint createProgram(std::string const& vert_shader_source_path,
-	                     std::string const& frag_shader_source_path);
+	//! \brief Overload of createTexture to allocate a cubemap texture.
+	GLuint createTextureCubeMap(uint32_t width, uint32_t height,
+	                            GLint internal_format = GL_RGBA,
+	                            GLenum format = GL_RGBA,
+	                            GLenum type = GL_UNSIGNED_BYTE,
+	                            GLvoid const* data = nullptr);
 
-	//! \brief Display the current texture in the specified rectangle.
-	//!
-	//! @param [in] lower_left the lower left corner of the rectangle
-	//!             containing the texture
-	//! @param [in] upper_right the upper rigth corner of the rectangle
-	//!             containing the texture
-	//! @param [in] texture the OpenGL name of the texture to display
-	//! @param [in] sampler the OpenGL name of the sampler to use
-	//! @param [in] swizzle how to mix in the different channels, for
-	//!             example (0, 2, 1, -1) will swap the green and blue
-	//!             channels as well as invalidating (setting it to 1) the
-	//!             alpha channel
-	//! @param [in] window_size the size in pixels of the main window, the
-	//!             one relative to which you want to draw this texture
-	//! @param [in] linearise whether the given texture should be
-	//!             linearised using the provided |nearPlane| and
-	//!             |farPlane|.
-	//! @param [in] nearPlane the near plane used when linearising depth
-	//!             textures; it is ignored if |linearise| is false.
-	//! @param [in] farPlane the far plane used when linearising depth
-	//!             textures; it is ignored if |linearise| is false.
-	void displayTexture(glm::vec2 const& lower_left,
-	                    glm::vec2 const& upper_right, GLuint texture,
-	                    GLuint sampler, glm::ivec4 const& swizzle,
-	                    glm::ivec2 const& window_size, bool linearise = false,
-	                    float nearPlane = 0.0f, float farPlane = 0.0f);
+	//! \brief Display a shader build error.
+	void displayShaderError(std::string const& message);
 
-	//! \brief Create an OpenGL FrameBuffer Object using the specified
-	//!        attachments.
-	//!
-	//! @param [in] color_attachments a vector of all the texture to bind
-	//!             as color attachment, i.e. not as depth texture
-	//! @param [in] depth_attachment a texture, if any, to use as depth
-	//!             attachment
-	//! @return the name of the OpenGL FBO
-	GLuint createFBO(std::vector<GLuint> const& color_attachments,
-	                 GLuint depth_attachment = 0u);
+	//! \brief Display a link error.
+	void displayLinkError(std::string const& message);
 
-	//! \brief Create an OpenGL sampler and set it up.
-	//!
-	//! @param [in] setup a lambda function to parameterise the sampler
-	//! @return the name of the OpenGL sampler
-	GLuint createSampler(std::function<void (GLuint)> const& setup);
-
-	//! \brief Draw full screen.
-	void drawFullscreen();
-
-	//! \brief Retrieve the ID of a small placeholder texture.
-	GLuint getDebugTextureID();
-
-	//! \brief Render a right-hand orthonormal basis.
-	//!
-	//! @param [in] thickness_scale By how much to scale the thickness of the axes
-	//! @param [in] length_scale By how much to scale the length of the axes
-	//! @param [in] view_projection Matrix transforming from world-space to clip-space
-	//! @param [in] world Matrix transforming from model-space to
-	//!             world-space
-	void renderBasis(float thickness_scale, float length_scale, glm::mat4 const& view_projection, glm::mat4 const& world = glm::mat4(1.0f));
-
-	//! \brief Add a combo box to the current ImGUI window, to choose a
-	//!        cull mode.
-	//!
-	//! @param [in] label Text to be displayed near the combo box.
-	//! @param [inout] cull_mode The currently selected cull mode,
-	//!                which will be modified to contain the newly selected
-	//!                one.
-	//! @return whether the selection was changed
-	bool uiSelectCullMode(std::string const& label, enum cull_mode_t& cull_mode) noexcept;
-
-	//! \brief Enable or disable culling, and call glCullFace with the
-	//!        specified cull mode.
-	void changeCullMode(enum cull_mode_t const cull_mode) noexcept;
-
-	//! \brief Add a combo box to the current ImGUI window, to choose a
-	//!        polygon mode.
-	//!
-	//! @param [in] label Text to be displayed near the combo box.
-	//! @param [inout] polygon_mode The currently selected polygon mode,
-	//!                which will be modified to contain the newly selected
-	//!                one.
-	//! @return whether the selection was changed
-	bool uiSelectPolygonMode(std::string const& label, enum polygon_mode_t& polygon_mode) noexcept;
-
-	//! \brief Call glPolygonMode for both front and back faces, with the
-	//!        specified polygon mode.
-	void changePolygonMode(enum polygon_mode_t const polygon_mode) noexcept;
+	//! \brief Creates one fullscreen quadrilateral mesh.
+	mesh_data createFullscreenQuad();
 }
