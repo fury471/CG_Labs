e45c7c8de529908e5421b8326bce4dfc89777761
Fury <fu.yu.447711@gmail.com>
2026-05-29 22:46:21 +0200

    sandbox: add clear pass interface

---
+++ src/sandbox/gfx/ClearPass.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <array>
+
+namespace sfm::gfx
+{
+
+class ClearPass final
+{
+public:
+	using Colour = std::array<float, 4>;
+
+	explicit ClearPass(Colour colour) noexcept;
+
+	void initialise() const noexcept;
+	void set_colour(Colour colour) noexcept;
+	[[nodiscard]] Colour const& colour() const noexcept;
+	void render() const noexcept;
+
+private:
+	Colour m_colour;
+};
+
+} // namespace sfm::gfx
