ab56d2730f9c55db38738a895658b054f3c2e769
Fury <fu.yu.447711@gmail.com>
2026-05-30 00:06:35 +0200

    gfx: implement move-only sampler owner

---
+++ src/sandbox/gfx/Sampler.cpp
@@ -0,0 +1,44 @@
+#include "Sampler.hpp"
+
+#include "DebugLabel.hpp"
+
+#include <utility>
+
+namespace sfm::gfx
+{
+
+Sampler::Sampler(std::string_view debug_label) noexcept
+{
+	glCreateSamplers(1, &m_id);
+	detail::label_object(GL_SAMPLER, m_id, debug_label);
+}
+
+Sampler::~Sampler() noexcept
+{
+	reset();
+}
+
+Sampler::Sampler(Sampler&& other) noexcept
+	: m_id(std::exchange(other.m_id, 0u))
+{
+}
+
+Sampler& Sampler::operator=(Sampler&& other) noexcept
+{
+	if (this != &other) {
+		reset();
+		m_id = std::exchange(other.m_id, 0u);
+	}
+	return *this;
+}
+
+void Sampler::reset() noexcept
+{
+	if (m_id == 0u)
+		return;
+
+	glDeleteSamplers(1, &m_id);
+	m_id = 0u;
+}
+
+} // namespace sfm::gfx
